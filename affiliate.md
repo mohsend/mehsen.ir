056a06a1578d624a57b95d4fa4697b49ef4acaa5
Mohsen Dastjerdi Zade <me@mehsen.com>
2020-04-19 17:12:12 +0430

    make digikala links sharable

--- affiliate.md
+++ affiliate.md
@@ -12,7 +12,7 @@ title: لینک‌های معرف
 
 [دیجی‌کالا](https://affstat.adro.co/click/169768e2-8920-4a25-a90b-de97baf7dc48/aHR0cHM6Ly93d3cuZGlnaWthbGEuY29t) به عنوان یکی از قدیمی‌ترین فروشگاه های اینترنتی با بیش از یک دهه تجربه، با پایبندی به سه اصل، پرداخت در محل، 7 روز ضمانت بازگشت کالا و تضمین اصل‌بودن کالا موفق شده تا همگام با فروشگاه‌های معتبر جهان، به بزرگ‌ترین فروشگاه اینترنتی ایران تبدیل شود.
 
-### تولید لینک دلخواه خودتان
+### [تولید لینک دلخواه خودتان](/buy/digikala)
 <div style="width: 100%;">
 <div style="direction: ltr; width: 70%; float: left;">
 <input style="width: 100%;" type="text" placeholder="https://digikala.com/..." id="url" name="url" value="">
--- assets/affiliate.js
+++ assets/affiliate.js
@@ -1,22 +1,57 @@
-function encode(open) {
+function loadlist() {
+    listString = window.location.hash.substring(1);
+    productIds = listString.split(',');
+    productIds.forEach(function(el) {
+        var url = "https://www.digikala.com/product/" + el;
+        add_to_list(url, url);
+      });
+}
+
+function make_affiliate(url) {
     const base = "https://affstat.adro.co/click/169768e2-8920-4a25-a90b-de97baf7dc48/";
+    var affiliate = base + window.btoa(url);
+    return affiliate;
+}
+
+function add_to_hash(url) {
+    var productid = url.match(/dkp-\d+/);
+    if (window.location.hash) {
+        window.location.hash += ',' + productid; 
+    } else {
+        window.location.hash += productid;
+    }
+}
+
+function add_to_list(url, fullUrl) {
+    // add product id to url hash
+    var affiliate = make_affiliate(url)
+    // add to ul
+    var parent = document.getElementById('afflinks');
+    var node = document.createElement("LI");
+    var linknode = document.createElement("A");
+    var textnode = document.createTextNode(decodeURI(fullUrl));
+    linknode.href = affiliate;
+    linknode.appendChild(textnode);
+    node.appendChild(linknode);
+    parent.appendChild(node);
+    return affiliate;
+}
+
+function encode(open) {
     var url = document.getElementById('url').value.trim();
     try {
         var producturl = url.match(/https?:\/\/(www\.)?digikala\.com\/product\/dkp-\d+/)[0];
         document.getElementById('url').value = '';
-        var affiliate = base + window.btoa(producturl);
-        var parent = document.getElementById('afflinks')
-        var node = document.createElement("LI");
-        var linknode = document.createElement("A");
-        var textnode = document.createTextNode(decodeURI(url));
-        linknode.href = affiliate;
-        linknode.appendChild(textnode);
-        node.appendChild(linknode);
-        parent.appendChild(node);
+        affiliate = add_to_list(producturl, url);
+        add_to_hash(producturl);
         if (open == '_blank') {
             window.open(affiliate, '_blank');
         }
     } catch {
         alert("به نظر میاد لینک کالای دیجیکالا نیست. -ـ-");
     }
+}
+
+if (window.location.hash) {
+    loadlist();
 }
